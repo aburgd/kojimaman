52d9d9f4a7a7792f7400317b98bc5a50bd3c1f96
Alec Burgdorf <aeburgd@gmail.com>
2020-03-12 13:36:28 -0500

    Finish main.rs
    too much to explain; add fmt::Display, add comments and section headers, add type annotation shorthands, ignore \n in line readings (windows, i'm looking at you)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use colored::*;
 use std::collections::HashMap;
 use std::fmt;
-use std::thread;
 use text_io::read;
 use tiny_die::Die;
 
-// SHORTHAND
+// COLLECTIONS SHORTHAND
 type Information = HashMap<String, String>;
 type Conditions = Vec<Condition>;
+type Names = HashMap<Category, String>;
 
 pub enum Condition {
     None,
@@ -22,7 +22,7 @@ pub enum Condition {
 #[derive(PartialEq, Eq, Hash, Clone)]
 pub enum Category {
     Normal,
-    Occupation,
+    Occupational,
     Horny,
     The,
     Cool,
@@ -32,51 +32,71 @@ pub enum Category {
 
 impl fmt::Display for Category {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
+        match *self {
+            Category::Normal => write!(f, "Normal"),
+            Category::Occupational => write!(f, "Occupational"),
+            Category::Horny => write!(f, "Horny"),
+            Category::The => write!(f, "The"),
+            Category::Cool => write!(f, "Cool"),
+            Category::Violent => write!(f, "Violent"),
+            Category::LacksSubtext => write!(f, "Lacks Subtext"),
+        }
+    }
+}
+
+impl fmt::Display for Condition {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Condition::Man => write!(f, "Man"),
+            Condition::Big => write!(f, "Big"),
+            Condition::Old => write!(f, "Old"),
+            Condition::Condition => write!(f, "Condition"),
+            Condition::Clone => write!(f, "Clone"),
+            Condition::Kojima => write!(f, "Kojima"),
+            Condition::None => write!(f, "None"),
+        }
+    }
+}
+
+fn wait() {
+    let drop: String = read!("{}\r\n");
+    if drop.contains("\n") {
+        std::process::exit(0);
     }
 }
 
 fn main() {
     // BEGINNING SECTIONS
-    // let how_many: u8 = section_how_many_names();
-    let true_category: Category = thread::spawn(|| section_name_category()).join().unwrap();
-    println!("{}", true_category);
-    // let conditions: Conditions = section_determine_name_conditions();
+    let how_many: u8 = section_how_many_names();
+    let true_category: Category = section_name_category();
+    let conditions: Conditions = section_determine_name_conditions();
     // COLLECTION SECTIONS
-    // let personal: Information = section_personal_information();
-    // let kojima: Information = section_kojima_information();
+    let personal: Information = section_personal_information();
+    let kojima: Information = section_kojima_information();
     // GENERATION SECTIONS
-    // TODO: Fix this overflow
     // Find true_name
-    // thread::spawn(move || {
-    //     let true_name = find_true_name(&true_category, &personal, &kojima, &conditions);
-    //     println!("{}: {}", true_category, true_name);
-    // });
+    let true_name = find_true_name(&true_category, &personal, &kojima, &conditions);
     // If 1 + 6 alternate, must have a true name.
     // all_names should contain true_name, but only do it if required
-    // if how_many == 1 {
-    //     println!("{}: {}", true_category, true_name);
-    // } else {
-    //     let mut all_names: HashMap<Category, String> =
-    //         find_all_names(&personal, &kojima, &conditions);
-    //     println!("True Name\n=========");
-    //     println!("{}: {}", true_category, true_name);
-    //     all_names.remove_entry(&true_category);
-    //     println!("Alternates\n==========");
-    //     for name in all_names {
-    //         println!("{}: {}", name.0, name.1);
-    //     }
-    // }
+    if how_many == 1 {
+        println!("{}: {}", true_category, true_name);
+    } else {
+        let mut all_names: Names = find_all_names(&personal, &kojima, &conditions);
+        println!("{}", "True Name\n=========".bold());
+        println!("{}: {}\n", true_category, true_name);
+        all_names.remove(&true_category);
+        println!("{}", "Alternates\n==========".bold());
+        for name in all_names {
+            println!("{}: {}", name.0, name.1);
+        }
+    }
+    wait();
 }
 
-fn find_all_names(
-    personal: &Information,
-    kojima: &Information,
-    conditions: &Conditions,
-) -> HashMap<Category, String> {
+fn find_all_names(personal: &Information, kojima: &Information, conditions: &Conditions) -> Names {
     let categories: Vec<Category> = vec![
         Category::Normal,
-        Category::Occupation,
+        Category::Occupational,
         Category::Horny,
         Category::The,
         Category::Cool,
@@ -87,7 +107,7 @@ fn find_all_names(
     for category in categories {
         match category {
             Category::Normal => all_names.insert(category, section_normal(personal, conditions)),
-            Category::Occupation => {
+            Category::Occupational => {
                 all_names.insert(category, section_occupational(personal, kojima, conditions))
             }
             Category::Horny => all_names.insert(category, section_horny(personal, conditions)),
@@ -138,7 +158,7 @@ fn find_true_name(
 ) -> String {
     match name_category {
         Category::Normal => section_normal(&personal, &conditions),
-        Category::Occupation => section_occupational(&personal, &kojima, &conditions),
+        Category::Occupational => section_occupational(&personal, &kojima, &conditions),
         Category::Horny => section_horny(&personal, &conditions),
         Category::The => section_the(&personal, &kojima, &conditions),
         Category::Cool => section_cool(&personal, &kojima, &conditions),
@@ -160,65 +180,65 @@ fn section_how_many_names() -> u8 {
 fn section_personal_information() -> Information {
     let mut personal_information: Information = HashMap::new();
     println!("{} ", "What is your full name?".bold());
-    personal_information.insert("full_name".to_string(), read!("{}\r"));
+    personal_information.insert("full_name".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is your occupation? Summarize in one -er verb.".bold()
     );
-    personal_information.insert("one_word_occupation".to_string(), read!("{}\r"));
+    personal_information.insert("one_word_occupation".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What was your first pet’s specific species/breed? If you've never had a pet, please answer with an animal you wish you owned.".bold()
     );
-    personal_information.insert("first_pet_breed".to_string(), read!("{}\r"));
+    personal_information.insert("first_pet_breed".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What’s your most embarrassing childhood memory? Summarise it with 2 words.".bold()
     );
-    personal_information.insert("two_word_memory".to_string(), read!("{}\r"));
+    personal_information.insert("two_word_memory".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is the object you'd least like to be stabbed by?".bold()
     );
-    personal_information.insert("least_like_stabbed".to_string(), read!("{}\r"));
+    personal_information.insert("least_like_stabbed".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is something you're good at? Summarize in one -ing verb.".bold()
     );
-    personal_information.insert("good_at".to_string(), read!("{}\r"));
+    personal_information.insert("good_at".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "How many carrots do you believe you could eat in one sitting, if someone, like, forced you to eat as many carrots as possible?".bold()
     );
-    personal_information.insert("carrots_one_sitting".to_string(), read!("{}\r"));
+    personal_information.insert("carrots_one_sitting".to_string(), read!("{}\r\n"));
     println!("{} ", "What is your greatest intangible fear?".bold());
-    personal_information.insert("intangible_fear".to_string(), read!("{}\r"));
+    personal_information.insert("intangible_fear".to_string(), read!("{}\r\n"));
     println!("{} ", "What is your greatest tangible fear?".bold());
-    personal_information.insert("tangible_fear".to_string(), read!("{}\r"));
+    personal_information.insert("tangible_fear".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
-        "What is the last thing you did before starting this worksheet".bold()
+        "What is the last thing you did before starting this worksheet?".bold()
     );
-    personal_information.insert("last_thing".to_string(), read!("{}\r"));
+    personal_information.insert("last_thing".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "In one word, what condition is your body currently in?".bold()
     );
-    personal_information.insert("body_condition".to_string(), read!("{}\r"));
+    personal_information.insert("body_condition".to_string(), read!("{}\r\n"));
     println!("{} ", "What is your favorite state of matter?".bold());
-    personal_information.insert("favorite_state_matter".to_string(), read!("{}\r"));
+    personal_information.insert("favorite_state_matter".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is a word your name sort of sounds like?".bold()
     );
-    personal_information.insert("name_sounds_like".to_string(), read!("{}\r"));
+    personal_information.insert("name_sounds_like".to_string(), read!("{}\r\n"));
     println!("{} ", "What is your Zodiac sign?".bold());
-    personal_information.insert("zodiac".to_string(), read!("{}\r"));
+    personal_information.insert("zodiac".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "If you could define your personality in one word, what would it be?".bold()
     );
-    personal_information.insert("one_word_personality".to_string(), read!("{}\r"));
+    personal_information.insert("one_word_personality".to_string(), read!("{}\r\n"));
     return personal_information;
 }
 
@@ -229,32 +249,32 @@ fn section_kojima_information() -> Information {
         "{} ",
         "Who is your favorite film character? Must be played by Kurt Russell.".bold()
     );
-    kojima_information.insert("favorite_kurt_russell".to_string(), read!("{}\r"));
+    kojima_information.insert("favorite_kurt_russell".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is the last word of the title of your favorite Kubrick film?".bold()
     );
-    kojima_information.insert("favorite_kubrick_film".to_string(), read!("{}\r"));
+    kojima_information.insert("favorite_kubrick_film".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is the first word in the title of your favorite Joy Division album?".bold()
     );
-    kojima_information.insert("favorite_joy_division".to_string(), read!("{}\r"));
+    kojima_information.insert("favorite_joy_division".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is a scientific term you picked up recently?".bold()
     );
-    kojima_information.insert("recent_scientfic_term".to_string(), read!("{}\r"));
+    kojima_information.insert("recent_scientfic_term".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is a piece of military hardware you think looks cool, even though war is bad?".bold()
     );
-    kojima_information.insert("cool_military_war_bad".to_string(), read!("{}\r"));
+    kojima_information.insert("cool_military_war_bad".to_string(), read!("{}\r\n"));
     println!(
         "{} ",
         "What is something you'd enjoy watching Mads Mikkelsen do? Summarize in one word.".bold()
     );
-    kojima_information.insert("mads_mikkelsen_amazing".to_string(), read!("{}\r"));
+    kojima_information.insert("mads_mikkelsen_amazing".to_string(), read!("{}\r\n"));
     return kojima_information;
 }
 
@@ -262,29 +282,25 @@ fn section_kojima_information() -> Information {
 fn section_determine_name_conditions() -> Conditions {
     let dee_four: Die = Die::new(4);
     let man_condition: Condition = match dee_four.roll() {
-        1..=3 => Condition::None,
         4 => Condition::Man,
-        _ => Condition::None, // TODO: figure out how to eliminate this step; the tiny_die crate ensures rolls are within bounds
+        1..=3 | _ => Condition::None,
     };
     let dee_eight: Die = Die::new(8);
     let condition_condition: Condition = match dee_eight.roll() {
-        1..=5 => Condition::None,
         6 => Condition::Big,
         7 => Condition::Old,
         8 => Condition::Condition,
-        _ => Condition::None,
+        1..=5 | _ => Condition::None,
     };
     let dee_twelve: Die = Die::new(12);
     let clone_condition: Condition = match dee_twelve.roll() {
-        1..=11 => Condition::None,
         12 => Condition::Clone,
-        _ => Condition::None,
+        1..=11 | _ => Condition::None,
     };
     let dee_hundred: Die = Die::new(100);
     let kojima_condition: Condition = match dee_hundred.roll() {
-        1..=68 | 70..=100 => Condition::None,
         69 => Condition::Kojima,
-        _ => Condition::None,
+        1..=68 | 70..=100 | _ => Condition::None,
     };
     vec![
         man_condition,
@@ -296,9 +312,10 @@ fn section_determine_name_conditions() -> Conditions {
 
 // Section 5
 fn section_name_category() -> Category {
-    match Die::new(20).roll() {
+    let dee_twenty = Die::new(20);
+    match dee_twenty.roll() {
         1 => Category::Normal,
-        2..=6 => Category::Occupation,
+        2..=6 => Category::Occupational,
         7..=10 => Category::Horny,
         11..=13 => Category::The,
         14..=17 => Category::Cool,
